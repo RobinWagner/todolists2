5f6af4fa35a6e6628a39b8c3819356f5a3051a38
Robin <robin-wagner@web.de>
2015-11-07 22:20:23 +0100

    Add validations for profiles and users

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,19 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_or_last_name_nil
+  validates :gender, inclusion: { in: ["male", "female"] }
+  validate :male_has_name_sue
+
+  def first_or_last_name_nil
+    if first_name.nil? && last_name.nil?
+      errors.add(:last_name, "cannot both be nil!")
+    end
+  end
+
+  def male_has_name_sue
+    if gender == "male" && first_name == "Sue"
+      errors.add(:first_name, "not a male name!")
+    end
+  end
 end
